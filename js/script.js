458fa5cd9161e1bd28ad33b3ce320185eb5c0557
Nurali Davlatov <nur3.dav.97@gmail.com>
2024-01-06 01:36:53 +0500

    new changes

--- js/script.js
+++ js/script.js
@@ -21,6 +21,17 @@ $(document).ready(function () {
         });
     }
 
+    $('.dropdown__btn').on('click', function () {
+        $('.dropdown__text').slideUp();
+        if ($(this).hasClass('active')) {
+            $('.dropdown__btn, .dropdown__text').removeClass('active');
+        } else {
+            $('.dropdown__btn, .dropdown__text').removeClass('active');
+            $(this).addClass('active');
+            $(this).next('.dropdown__text').slideDown();
+        }
+    })
+
     $('a[href*="#"]').on('click', function (e) {
         e.preventDefault();
         $('html, body').animate({ scrollTop: $($(this).attr('href')).offset().top, }, 300,)
